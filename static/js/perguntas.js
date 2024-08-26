a23a85cac836d64cda1430781f1d2d2646f0a08e
Lenon Yuri <lenonyurisilva26@gmail.com>
2024-08-26 00:31:38 -0300

    Initial commit

---
+++ static/js/perguntas.js
@@ -0,0 +1,26 @@
+// Vetores com palavras e dicas
+const palavras = [
+  "computador",
+  "internet",
+  "programação",
+  "algoritmo",
+  "hardware",
+  "software",
+  "navegador",
+  "banco de dados",
+  "rede",
+  "criptografia",
+];
+
+const dicas = [
+  "Dispositivo eletrônico para processamento de dados",
+  "Rede mundial de computadores",
+  "Processo de criação de software",
+  "Sequência de passos para resolver um problema",
+  "Componentes físicos do computador",
+  "Programas e sistemas operacionais",
+  "Aplicativo para acessar a web",
+  "Armazenamento organizado de dados",
+  "Conjunto de computadores interconectados",
+  "Método de proteger informações através de codificação",
+];
